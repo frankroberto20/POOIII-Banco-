b6e9432fc59207c5e11e9dd8ad1aedc4fd3fd283
Raul Sanchez <raulenrique6@hotmail.com>
2019-10-05 15:11:31 -0400

    Created base Request and Response classes

---
+++ CapaIntegracion/RequestHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public abstract class RequestHeader
+{
+    private int CuentaOrigen;
+    private int Cedula;
+    private decimal Monto;
+    private DateTime dateTime;
+
+	public RequestHeader()
+	{
+        
+	}
+}
+
+public class InterbankRequest : RequestHeader
+{
+    private string BancoReceptor;
+    private int CuentaReceptora;
+}
