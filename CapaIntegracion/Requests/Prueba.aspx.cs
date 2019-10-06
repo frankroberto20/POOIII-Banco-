c3c6514bed5e0d38633a613813902ca4c7d7bb31
calt08 <cesarlopez2808@gmail.com>
2019-10-06 19:17:28 -0400

    ds, azure, pruebas

---
+++ CapaIntegracion/Requests/Prueba.aspx.cs
@@ -0,0 +1,28 @@
+﻿using CapaIntegracion.IntegracionDSTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CapaIntegracion.Requests
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //SIMPLEMENTE PRUEBA Para probar azure y dataset
+
+            PruebaTableAdapter ts = new PruebaTableAdapter();
+            ts.Insert("Cesar");
+            
+
+        }
+    }
+}
